8da1e86ea66e361e58eb688ea77e05e17f1d51f4
basha-github <sharifabee.alur@gmail.com>
2024-09-26 10:48:14 +0530

    v3

---
+++ ListDemo3.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo3 {
+
+	public static void main(String[] args) {
+		
+		
+		List data = new ArrayList();
+		
+		data.add(100);
+		data.add(100);
+		data.add(100);
+		data.add(100);
+		data.add(null);
+		data.add(null);
+		data.add(null);
+		
+		
+		
+		System.out.println(data);
+		
+		
+
+	}
+
+}
